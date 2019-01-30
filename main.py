5109a311606c437b4e3be4c944e1185bca5eac57
nhann <nhanndeh@yahoo.com>
2019-01-30 08:53:55 -0600

    Added blank lines

--- main.py
+++ main.py
@@ -52,18 +52,22 @@ def show_read_books():
 
 def show_unread_books():
     unread_books = store.get_books_by_read_value(False)
+    print("\n")  # adding blank line before list of books
     ui.show_books(unread_books)
+    print("\n")  # adding blank line before list of books
 
 
 def show_all_books():
     books = store.get_all_books(
-    print("\n"))
+    print("\n"))# adding blank line before list of books
     ui.show_books(books)
-    print("\n")
+    print("\n")# adding blank line before list of books
 
 def search_book():
     search_term = ui.ask_question('Enter search term, will match partial authors or titles.')
+    print("\n")  # adding blank line before list of books
     matches = store.book_search(search_term)
+    print("\n")  # adding blank line before list of books
     ui.show_books(matches)
 
 
