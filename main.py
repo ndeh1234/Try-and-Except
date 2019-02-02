153bf4d5d363ae28ba19404d8d450b6734437899
nhann <nhanndeh@yahoo.com>
2019-02-02 14:08:14 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bookstore import BookStore
-                
+
 from menu import Menu
 import ui
 
@@ -73,11 +73,12 @@ def search_book():
 
 def change_read():
     book_id = ui.get_book_id()
-    try:
+    try:                               #Try statement will search the store to get the book name by its book_id
         book = store.get_book(book_id)
-    except:
+    except:                            # If book is not found or the book_id is incorrect etc, then the 'except' block will be executed
         ui.message('The book is not in Store!')
-    else:
+    else:                              #else as the book name will be retrieved from the store
+                                       #it will be certain that the book exists, hence the book_read_value() will be retrieved.
         new_read = ui.get_read_value()
         store.set_book_read(book_id, new_read)
 
